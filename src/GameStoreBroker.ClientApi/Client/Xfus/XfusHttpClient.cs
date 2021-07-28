ce239e5d4f827dd9c54c60c59c7408abedd01793
Alberto Baron <albaron@microsoft.com>
2021-07-28 16:05:53 +0100

    Removed file comments

--- src/GameStoreBroker.ClientApi/Client/Xfus/XfusHttpClient.cs
+++ src/GameStoreBroker.ClientApi/Client/Xfus/XfusHttpClient.cs
@@ -7,20 +7,6 @@ namespace GameStoreBroker.ClientApi.Client.Xfus
     {
         public XfusHttpClient(ILogger<XfusHttpClient> logger, HttpClient httpClient) : base(logger, httpClient)
         {
-            // will be config driven when features implemented
-
-            //httpClient.BaseAddress = new Uri(xfusUploadInfo.UploadDomain + "/api/v2/assets/");
-            //httpClient.Timeout = TimeSpan.FromMilliseconds(this.uploadConfig.HttpUploadTimeoutMs);
-            //httpClient.DefaultRequestHeaders.Add("Tenant", config.Tenant);
-            //var authToken = Convert.ToBase64String(Encoding.UTF8.GetBytes(xfusUploadInfo.Token));
-
-            //httpClient.BaseAddress = new Uri("/api/v2/assets/");
-            //httpClient.Timeout = TimeSpan.FromSeconds(10);
-
-            //var authToken = Convert.ToBase64String(Encoding.UTF8.GetBytes("mytoken"));
-
-            //httpClient.DefaultRequestHeaders.Add("Authorization", authToken);
-            //httpClient.DefaultRequestHeaders.Add("Tenant", "Xfus");
         }
     }
 }
