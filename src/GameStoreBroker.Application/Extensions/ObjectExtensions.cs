73d5d6cc9a5b5f9b390b4ec6e8c1ec961d559528
Alberto Baron <albaron@microsoft.com>
2021-09-02 21:03:25 +0100

    Removed unused method

--- src/GameStoreBroker.Application/Extensions/ObjectExtensions.cs
+++ src/GameStoreBroker.Application/Extensions/ObjectExtensions.cs
@@ -2,16 +2,13 @@
 // Licensed under the MIT License.
 
 using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
 using System.Text.Json;
 
 namespace GameStoreBroker.Application.Extensions
 {
     internal static class ObjectExtensions
     {
-        private static readonly JsonSerializerOptions DefaultJsonSerializerOptions = new JsonSerializerOptions()
+        private static readonly JsonSerializerOptions DefaultJsonSerializerOptions = new ()
         {
             IgnoreNullValues = true,
             PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
@@ -19,7 +16,7 @@ namespace GameStoreBroker.Application.Extensions
 
         public static string ToJson<T>(this T value, JsonSerializerOptions jsonSerializerOptions = null) where T : class
         {
-            if (value == null)
+            if (value is null)
             {
                 return "null";
             }
@@ -33,17 +30,6 @@ namespace GameStoreBroker.Application.Extensions
             {
                 return $"Could not serialize object to json - {ex.Message}";
             }
-        }
-
-        public static bool IsValid<T>(this T objectToValidate, out IEnumerable<string> validationErrors)
-        {
-            validationErrors = null;
-            var validationResults = new List<ValidationResult>();
-
-            var isValid = Validator.TryValidateObject(objectToValidate, new ValidationContext(objectToValidate), validationResults, true);
-
-            validationErrors = validationResults.Select(x => x.ErrorMessage);
-            return isValid;
         }
     }
 }
