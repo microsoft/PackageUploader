a5461dd229077465ba31af2b7d457ec283d4b3ef
Alberto Baron <albaron@microsoft.com>
2021-09-13 09:11:49 +0100

    added nextLink property to pagedCollection

--- src/GameStoreBroker.ClientApi/Client/Ingestion/Models/Internal/PagedCollection.cs
+++ src/GameStoreBroker.ClientApi/Client/Ingestion/Models/Internal/PagedCollection.cs
@@ -5,11 +5,16 @@ using System.Collections.Generic;
 
 namespace GameStoreBroker.ClientApi.Client.Ingestion.Models.Internal
 {
-    public sealed class PagedCollection<T>
+    internal sealed class PagedCollection<T>
     {
         /// <summary>
         /// Gets the current page of elements.
         /// </summary>
         public IList<T> Value { get; set; }
+
+        /// <summary>
+        /// Location URI for the next page (if applicable). If there is no next page, the property is not returned
+        /// </summary>
+        public string NextLink { get; set; }
     }
 }
