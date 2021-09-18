21737dddd2572a6251e69b5696e301f992eea800
Alberto Baron <albaron@microsoft.com>
2021-09-18 22:33:22 +0100

    added missing copyright

--- src/GameStoreBroker.ClientApi/Client/Ingestion/Models/GamePackageResource.cs
+++ src/GameStoreBroker.ClientApi/Client/Ingestion/Models/GamePackageResource.cs
@@ -1,4 +1,5 @@
-﻿// Copyright (C) Microsoft. All rights reserved.
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
 
 namespace GameStoreBroker.ClientApi.Client.Ingestion.Models
 {
