e3550330378b2f13ccaafedb90a7f07be2567069
Alberto Baron <albaron@microsoft.com>
2021-09-22 17:51:19 +0100

    Added submission missing properties internal model

--- src/GameStoreBroker.ClientApi/Client/Ingestion/Models/Internal/IngestionSubmission.cs
+++ src/GameStoreBroker.ClientApi/Client/Ingestion/Models/Internal/IngestionSubmission.cs
@@ -8,10 +8,13 @@ namespace GameStoreBroker.ClientApi.Client.Ingestion.Models.Internal
 {
     internal sealed class IngestionSubmission
     {
-        public string Id { get; set; }
+        /// <summary>
+        /// Resource type [Submission]
+        /// </summary>
+        public string ResourceType { get; set; }
 
         /// <summary>
-        /// (readonly) State of Submission [InProgress, Published]
+        /// (readonly) State of Submission [Inprogress, Published]
         /// </summary>
         public string State { get; set; }
 
@@ -20,16 +23,54 @@ namespace GameStoreBroker.ClientApi.Client.Ingestion.Models.Internal
         /// </summary>
         public string Substate { get; set; }
 
+        /// <summary>
+        /// Target of submission [{Sandbox, sandboxID}, {Flight, flightID}, {Scope, Preview/Live}]
+        /// </summary>
         public IList<TypeValuePair> Targets { get; set; }
 
+        /// <summary>
+        /// Source of Submission resources: [{Sandbox, sandboxID}] or [{Property, propertyInstanceID}, {Listing, listingInstanceID}, {Availability, availabilityInstanceID}, {Package, packageInstanceID}]
+        /// </summary>
         public IList<TypeValuePair> Resources { get; set; }
 
+        /// <summary>
+        /// Source Variants of submission
+        /// </summary>
+        public IList<IngestionVariantResource> VariantResources { get; set; }
+
+        /// <summary>
+        /// Submission publish details
+        /// </summary>
+        public IngestionPublishOption PublishOption { get; set; }
+
+        /// <summary>
+        /// Published time (UTC)
+        /// </summary>
         public DateTime? PublishedTimeInUtc { get; set; }
 
+        /// <summary>
+        /// Submission pending update status details
+        /// </summary>
         public IngestionPendingUpdateInfo PendingUpdateInfo { get; set; }
 
+        /// <summary>
+        /// Any extra information need to pass.
+        /// </summary>
+        public IList<TypeValuePair> ExtendedProperties { get; set; }
+
+        /// <summary>
+        /// (readonly) Release number
+        /// </summary>
         public int ReleaseNumber { get; set; }
 
+        /// <summary>
+        /// (readonly) Friendly name
+        /// </summary>
         public string FriendlyName { get; set; }
+
+        /// <summary>
+        /// Resource ID
+        /// </summary>
+        public string Id { get; set; }
     }
 }
