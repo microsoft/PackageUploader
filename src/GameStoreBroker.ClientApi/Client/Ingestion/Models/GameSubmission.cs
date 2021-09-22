b3a787fb4629c243901b7b05b561fb86738ebf72
Alberto Baron <albaron@microsoft.com>
2021-09-22 18:42:05 +0100

    Added comments to game submission model

--- src/GameStoreBroker.ClientApi/Client/Ingestion/Models/GameSubmission.cs
+++ src/GameStoreBroker.ClientApi/Client/Ingestion/Models/GameSubmission.cs
@@ -8,16 +8,34 @@ namespace GameStoreBroker.ClientApi.Client.Ingestion.Models
 {
     public class GameSubmission
     {
+        /// <summary>
+        /// Resource ID
+        /// </summary>
         public string Id { get; internal init; }
 
+        /// <summary>
+        /// State of the submission
+        /// </summary>
         public GameSubmissionState GameSubmissionState { get; internal init; }
 
+        /// <summary>
+        /// Release number
+        /// </summary>
         public int ReleaseNumber { get; internal init; }
 
+        /// <summary>
+        /// Friendly name
+        /// </summary>
         public string FriendlyName { get; internal init; }
 
+        /// <summary>
+        /// Published time (UTC)
+        /// </summary>
         public DateTime? PublishedTimeInUtc { get; internal init; }
 
+        /// <summary>
+        /// Submission validation items
+        /// </summary>
         public List<GameSubmissionValidationItem> SubmissionValidationItems { get; internal set; }
     }
 }
