a11376e5fb14ccd704bd7f1762cfe6b8e60befa5
Alberto Baron <albaron@microsoft.com>
2021-09-22 18:16:39 +0100

    Added submission enums

---
+++ src/GameStoreBroker.ClientApi/Client/Ingestion/Models/Internal/IngestionSubmissionSubstate.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace GameStoreBroker.ClientApi.Client.Ingestion.Models.Internal
+{
+    internal enum IngestionSubmissionSubstate
+    {
+        InDraft,
+        Submitted,
+        Cancelled,
+        Failed,
+        FailedInCertification,
+        ReadyToPublish,
+        Publishing,
+        Published,
+        InStore,
+    }
+}
