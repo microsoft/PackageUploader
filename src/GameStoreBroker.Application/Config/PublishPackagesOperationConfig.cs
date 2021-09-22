92b0c50932bf6f33131d950441a181c57341d1a6
Alberto Baron <albaron@microsoft.com>
2021-09-22 12:56:27 +0100

    fixed validation

--- src/GameStoreBroker.Application/Config/PublishPackagesOperationConfig.cs
+++ src/GameStoreBroker.Application/Config/PublishPackagesOperationConfig.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GameStoreBroker.Application.Config
 {
@@ -16,14 +12,11 @@ namespace GameStoreBroker.Application.Config
 
         protected override void Validate(IList<ValidationResult> validationResults)
         {
-            if (string.IsNullOrWhiteSpace(FlightName) || (string.IsNullOrWhiteSpace(BranchFriendlyName) && string.IsNullOrWhiteSpace(DestinationSandboxName)))
+            if ((!string.IsNullOrWhiteSpace(FlightName) || string.IsNullOrWhiteSpace(BranchFriendlyName) || string.IsNullOrWhiteSpace(DestinationSandboxName)) &&
+                (string.IsNullOrWhiteSpace(FlightName) || !string.IsNullOrWhiteSpace(BranchFriendlyName) || !string.IsNullOrWhiteSpace(DestinationSandboxName)))
             {
-                validationResults.Add(new ValidationResult($"{nameof(FlightName)} or ({nameof(BranchFriendlyName)} and {nameof(DestinationSandboxName)}) field is required.", new[] { nameof(FlightName), nameof(BranchFriendlyName), nameof(DestinationSandboxName) }));
-            }
-
-            if ((!string.IsNullOrWhiteSpace(DestinationSandboxName) || !string.IsNullOrWhiteSpace(BranchFriendlyName)) && !string.IsNullOrWhiteSpace(FlightName))
-            {
-                validationResults.Add(new ValidationResult($"Only one {nameof(BranchFriendlyName)} or {nameof(FlightName)} field is allowed. {nameof(DestinationSandboxName)} must be provided with {nameof(BranchFriendlyName)}", new[] { nameof(BranchFriendlyName), nameof(FlightName), nameof(DestinationSandboxName) }));
+                validationResults.Add(new ValidationResult($"{nameof(FlightName)} or ({nameof(BranchFriendlyName)} and {nameof(DestinationSandboxName)}) field is required.",
+                    new[] { nameof(FlightName), nameof(BranchFriendlyName), nameof(DestinationSandboxName) }));
             }
         }
     }
