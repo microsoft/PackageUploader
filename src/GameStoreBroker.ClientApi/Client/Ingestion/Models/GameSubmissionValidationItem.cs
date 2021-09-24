940414512090122ed8ecab56a0df575762e5025c
Alberto Baron <albaron@microsoft.com>
2021-09-24 09:56:42 +0100

    removed obvious comment

--- src/GameStoreBroker.ClientApi/Client/Ingestion/Models/GameSubmissionValidationItem.cs
+++ src/GameStoreBroker.ClientApi/Client/Ingestion/Models/GameSubmissionValidationItem.cs
@@ -6,14 +6,8 @@ namespace GameStoreBroker.ClientApi.Client.Ingestion.Models
     public sealed class GameSubmissionValidationItem
     {
         public string ErrorCode { get; internal init; }
-
-        /// <summary>
-        /// Severity for validation
-        /// </summary>
         public GameSubmissionValidationSeverity Severity { get; internal init; }
-
         public string Message { get; internal init; }
-
         public string Resource { get; internal init; }
     }
 }
