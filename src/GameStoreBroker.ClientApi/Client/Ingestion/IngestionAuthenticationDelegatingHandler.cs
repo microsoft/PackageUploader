1b27ea23e46e4e2f02676af6366a9a21cef5adf9
Alberto Baron <albaron@microsoft.com>
2021-11-05 16:10:59 +0000

    .

--- src/GameStoreBroker.ClientApi/Client/Ingestion/IngestionAuthenticationDelegatingHandler.cs
+++ src/GameStoreBroker.ClientApi/Client/Ingestion/IngestionAuthenticationDelegatingHandler.cs
@@ -24,17 +24,21 @@ namespace GameStoreBroker.ClientApi.Client.Ingestion
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken ct)
         {
             _token ??= await _accessTokenProvider.GetTokenAsync(ct);
-            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", _token.AccessToken);
-            var response = await base.SendAsync(request, ct);
+            var response = await SetAuthHeaderAndSendAsync(request, ct);
 
             if (response.StatusCode is HttpStatusCode.Unauthorized or HttpStatusCode.Forbidden)
             {
                 _token = await _accessTokenProvider.GetTokenAsync(ct);
-                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", _token.AccessToken);
-                response = await base.SendAsync(request, ct);
+                response = await SetAuthHeaderAndSendAsync(request, ct);
             }
 
             return response;
+        }
+
+        private async Task<HttpResponseMessage> SetAuthHeaderAndSendAsync(HttpRequestMessage request, CancellationToken ct)
+        {
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", _token.AccessToken);
+            return await base.SendAsync(request, ct);
         }
     }
 }
