ad56da0c2d706ff8515e6a6bee07f50aaf3ea91b
Seann Murdock 🐞 <semurdoc@microsoft.com>
2022-05-18 10:33:58 -0700

    update rest client to pass method of access header

--- src/PackageUploader.ClientApi/Client/Ingestion/Client/HttpRestClient.cs
+++ src/PackageUploader.ClientApi/Client/Ingestion/Client/HttpRestClient.cs
@@ -32,6 +32,7 @@ internal abstract class HttpRestClient : IHttpRestClient
 
     private static readonly MediaTypeHeaderValue JsonMediaTypeHeaderValue = new (MediaTypeNames.Application.Json);
     private const LogLevel VerboseLogLevel = LogLevel.Trace;
+    private const string SDKVersion = "SDK-V1.5.0";
 
     protected HttpRestClient(ILogger logger, HttpClient httpClient)
     {
@@ -161,6 +162,7 @@ internal abstract class HttpRestClient : IHttpRestClient
             request.Content = JsonContent.Create(inputValue, JsonMediaTypeHeaderValue, DefaultJsonSerializerOptions);
         }
         request.Headers.Add("Request-ID", Guid.NewGuid().ToString());
+        request.Headers.Add("MethodOfAccess", SDKVersion);
 
         if (customHeaders is not null && customHeaders.Any())
         {
