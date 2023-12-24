c6a0f32b8d5bc49c7b23b631aa6d7c215fe84ed8
Alberto Baron <albaron@microsoft.com>
2023-12-24 12:23:47 +0000

    made Filelogger aot/trim compatible

---
+++ src/PackageUploader.FileLogger/FileFormatterConfigureOptions.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace PackageUploader.FileLogger;
+
+/// <summary>
+/// Configures a FileFormatterOptions object from an IConfiguration.
+/// </summary>
+/// <remarks>
+/// Doesn't use ConfigurationBinder in order to allow ConfigurationBinder, and all its dependencies,
+/// to be trimmed. This improves app size and startup.
+/// </remarks>
+public class FileFormatterConfigureOptions : IConfigureOptions<FileFormatterOptions>
+{
+
+    private readonly IConfiguration _configuration;
+
+    public FileFormatterConfigureOptions(ILoggerProviderConfiguration<FileLoggerProvider> providerConfiguration)
+    {
+        _configuration = providerConfiguration.GetFormatterOptionsSection();
+    }
+
+    public void Configure(FileFormatterOptions options) => options.Configure(_configuration);
+}
---
+++ src/PackageUploader.FileLogger/FileLoggerConfigureOptions.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace PackageUploader.FileLogger
+{
+    /// <summary>
+    /// Configures a FileLoggerOptions object from an IConfiguration.
+    /// </summary>
+    /// <remarks>
+    /// Doesn't use ConfigurationBinder in order to allow ConfigurationBinder, and all its dependencies,
+    /// to be trimmed. This improves app size and startup.
+    /// </remarks>
+    internal sealed class FileLoggerConfigureOptions : IConfigureOptions<FileLoggerOptions>
+    {
+        private readonly IConfiguration _configuration;
+
+        public FileLoggerConfigureOptions(ILoggerProviderConfiguration<FileLoggerProvider> providerConfiguration)
+        {
+            _configuration = providerConfiguration.Configuration;
+        }
+
+        public void Configure(FileLoggerOptions options) => _configuration.Bind(options);
+    }
+}
