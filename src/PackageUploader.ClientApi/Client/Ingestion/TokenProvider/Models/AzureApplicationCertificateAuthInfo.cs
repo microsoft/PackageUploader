4ab45ed3f20fa424f1e63621ef3a14e556ce8d4d
Alberto Baron <albaron@microsoft.com>
2023-12-24 10:03:38 +0000

    adding OptionsValidator for Token Provider configs

--- src/PackageUploader.ClientApi/Client/Ingestion/TokenProvider/Models/AzureApplicationCertificateAuthInfo.cs
+++ src/PackageUploader.ClientApi/Client/Ingestion/TokenProvider/Models/AzureApplicationCertificateAuthInfo.cs
@@ -1,10 +1,15 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using Microsoft.Extensions.Options;
 using System.ComponentModel.DataAnnotations;
 using System.Security.Cryptography.X509Certificates;
 
 namespace PackageUploader.ClientApi.Client.Ingestion.TokenProvider.Models;
+
+[OptionsValidator]
+internal partial class AzureApplicationCertificateAuthInfoValidator : IValidateOptions<AzureApplicationCertificateAuthInfo>
+{ }
 
 public sealed class AzureApplicationCertificateAuthInfo : AadAuthInfo
 {
