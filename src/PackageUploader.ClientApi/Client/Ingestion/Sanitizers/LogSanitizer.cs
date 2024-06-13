1eb5cd4d2cc2f7b05cfae9bf17b841b98d161eb3
Alberto Baron <albaron@microsoft.com>
2024-06-13 11:04:16 +0100

    using GeneratedRegex

--- src/PackageUploader.ClientApi/Client/Ingestion/Sanitizers/LogSanitizer.cs
+++ src/PackageUploader.ClientApi/Client/Ingestion/Sanitizers/LogSanitizer.cs
@@ -5,18 +5,24 @@ using System.Text.RegularExpressions;
 
 namespace PackageUploader.ClientApi.Client.Ingestion.Sanitizers;
 
-public static class LogSanitizer
+public static partial class LogSanitizer
 {
+    [GeneratedRegex("\"token\":\\s*\"[^\"]+?([^\\/\"]+)\"")]
+    private static partial Regex ResponseRegex();
+
+    [GeneratedRegex("\"fileSasUri\":\\s*\"[^\"]+?([^\\/\"]+)\"")]
+    private static partial Regex SasRegex();
+
     public static string SanitizeJsonResponse(string jsonResponse)
     {
         if (string.IsNullOrWhiteSpace(jsonResponse))
             return jsonResponse;
 
         // Sanitizing token from responses
-        var responseBody = Regex.Replace(jsonResponse, "\"token\":\\s*\"[^\"]+?([^\\/\"]+)\"", "\"token\":\"REDACTED\"");
+        var responseBody = ResponseRegex().Replace(jsonResponse, "\"token\":\"REDACTED\"");
 
         // Sanitizing File Sas Uri from responses
-        var sasPropertyMatch = Regex.Match(responseBody, "\"fileSasUri\":\\s*\"[^\"]+?([^\\/\"]+)\"");
+        var sasPropertyMatch = SasRegex().Match(responseBody);
         if (sasPropertyMatch.Success)
             responseBody = responseBody.Replace(sasPropertyMatch.Groups[0].Value, sasPropertyMatch.Groups[0].Value.Split('?')[0] + "?REDACTED\"");
 
