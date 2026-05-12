f62798c13b8265df6b4105f3651bfccf1579bf47
Elshiekh Ahmed <elahmed@microsoft.com>
2026-05-12 14:29:50 -0700

    minor change: added spacing in progress bar text between "packaging using msixvc2..." and "x% complete"

--- src/PackageUploader.UI/Resources/Strings/PackagingProgress.Designer.cs
+++ src/PackageUploader.UI/Resources/Strings/PackagingProgress.Designer.cs
@@ -19,7 +19,7 @@ namespace PackageUploader.UI.Resources.Strings {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "18.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class PackagingProgress {
@@ -79,11 +79,29 @@ namespace PackageUploader.UI.Resources.Strings {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Packaging data....
+        /// </summary>
+        public static string PackagingDataText {
+            get {
+                return ResourceManager.GetString("PackagingDataText", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Getting your package ready....
         /// </summary>
         public static string PackagingProgressTitleText {
             get {
                 return ResourceManager.GetString("PackagingProgressTitleText", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Packaging using MSIXVC2....
+        /// </summary>
+        public static string PackingMsixvc2Text {
+            get {
+                return ResourceManager.GetString("PackingMsixvc2Text", resourceCulture);
             }
         }
         
@@ -106,15 +124,6 @@ namespace PackageUploader.UI.Resources.Strings {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Packaging using MSIXVC2....
-        /// </summary>
-        public static string PackingMsixvc2Text {
-            get {
-                return ResourceManager.GetString("PackingMsixvc2Text", resourceCulture);
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized string similar to Validating files....
         /// </summary>
         public static string ValidatingFilesText {
@@ -129,15 +138,6 @@ namespace PackageUploader.UI.Resources.Strings {
         public static string VerifyingPackageContentsText {
             get {
                 return ResourceManager.GetString("VerifyingPackageContentsText", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Packaging data....
-        /// </summary>
-        public static string PackagingDataText {
-            get {
-                return ResourceManager.GetString("PackagingDataText", resourceCulture);
             }
         }
     }
